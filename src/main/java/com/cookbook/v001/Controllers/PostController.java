746da1f0460945580c8da0121e0f30658d3a9572
Fernando Mendoza Olivares <fernando.emir.mendoza@gmail.com>
2020-08-07 23:34:28 -0500

    Changing name of post request mapping

--- src/main/java/com/cookbook/v001/Controllers/PostController.java
+++ src/main/java/com/cookbook/v001/Controllers/PostController.java
@@ -1,13 +1,14 @@
 package com.cookbook.v001.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/post")
+@RequestMapping("/posts")
 public class PostController {
 
-    @RequestMapping("/submit")
+    @PostMapping("/submit")
     public void submitPost(){
         System.out.println("Wow we got a post");
     }
